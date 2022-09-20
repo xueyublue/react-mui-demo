845e53e053f24de3f93127cec4952d3e7d97fa95
Xue Yu <xueyublue@gmail.com>
2022-09-20 12:03:00 +0800

    Added add modal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Button, Stack } from "@mui/material";
+import Add from "./components/Add";
 import Feed from "./components/Feed";
 import Navbar from "./components/Navbar";
 import Rightbar from "./components/Rightbar";
@@ -13,6 +14,7 @@ function App() {
         <Feed />
         <Rightbar />
       </Stack>
+      <Add />
     </div>
   );
 }
